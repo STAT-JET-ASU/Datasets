4d892bf76ce14b07fe6057eda5f03fa53948a82f
Jill Thomley <thomleyje@appstate.edu>
2021-11-15 19:52:23 -0500

    Add Kaggle datasets

---
+++ Kaggle/datalinks.R
@@ -0,0 +1,5 @@
+# https://www.kaggle.com/kemical/kickstarter-projects
+# https://www.kaggle.com/kkhandekar/billionaires-index
+# https://www.kaggle.com/residentmario/ramen-ratings
+# https://www.kaggle.com/shivamb/disney-movies-and-tv-shows
+# https://www.kaggle.com/shivamb/netflix-shows
