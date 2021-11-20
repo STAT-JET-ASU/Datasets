b3698a8ac86f27b5000140bb766c55d81bf108e1
Jill Thomley <thomleyje@appstate.edu>
2021-11-20 17:00:49 -0500

    Add new files to Kaggle folder

--- Kaggle/datalinks.R
+++ Kaggle/datalinks.R
@@ -3,3 +3,6 @@
 # https://www.kaggle.com/residentmario/ramen-ratings
 # https://www.kaggle.com/shivamb/disney-movies-and-tv-shows
 # https://www.kaggle.com/shivamb/netflix-shows
+
+
+# https://www.kaggle.com/ruchi798/tv-shows-on-netflix-prime-video-hulu-and-disney
