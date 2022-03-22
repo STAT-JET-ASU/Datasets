a0949a00fc99ef16abbc230ccc5556d432a21eab
Jill Thomley <thomleyje@appstate.edu>
2022-03-22 19:01:25 -0400

    Clean up repo

--- write_galton_file.R
+++
@@ -1,7 +0,0 @@
-galtonparentheights <- read.csv(file = "Instructor/galtonheightdata.csv") %>% 
-  group_by(Family) %>% 
-  summarize(Father = mean(Father),
-            Mother = mean(Mother)) %>% 
-  select(Father, Mother)
-
-# write.csv(galtonparentheights, file = "Instructor/galtonparentheights.csv", row.names = FALSE)
